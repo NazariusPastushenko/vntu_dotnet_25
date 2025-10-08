0f8238422eb68f6ea1cc016b08f0a2443fb86064
nazar <nazarelo2005@gmail.com>
2025-10-08 10:37:00 +0300

    First_Lab

---
+++ Domain.Models/WorkItem.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Model.Enums;
+
+namespace Domain.Model;
+
+public class WorkItem
+{
+    public DateTime CreationDate { get; set; }
+    public DateTime DueDate { get; set; }
+    public Priority Priority { get; set; }
+    public Complexity Complexity { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public bool IsCompleted { get; set; }
+
+    public override string ToString()
+    {
+        return $"{Title}: due {DueDate:dd.MM.yyyy}, {Priority.ToString().ToLower()} priority";
+    }
+}
